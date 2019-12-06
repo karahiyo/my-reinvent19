80ec72e35ff022b3e1a640b2ef86514b69aed3fe
Yusuke Shimizu <karahiyo@users.noreply.github.com>
2019-12-06 11:48:24 -0800

    import aws_embedded_metrics lib

--- serverless/cloudwatch-embedded-metrics-format/hello-cloudwatch-embedded-metrics-format/hello_world/app.py
+++ serverless/cloudwatch-embedded-metrics-format/hello-cloudwatch-embedded-metrics-format/hello_world/app.py
@@ -1,42 +1,21 @@
 import json
+from aws_embedded_metrics import metric_scope
 
-# import requests
+@metric_scope
+def lambda_handler(metrics, b):
 
+    print(metrics)
+    print(b)
 
-def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
+    metrics.put_dimensions({"Foo": "Bar"})
+    metrics.put_metric("ProcessingLatency", 100, "Milliseconds")
+    metrics.set_property("AccountId", "123456789012")
+    metrics.set_property("RequestId", "422b1569-16f6-4a03")
+    metrics.set_property("DeviceId", "61270781-c6ac-46f1")
 
     return {
         "statusCode": 200,
         "body": json.dumps({
             "message": "hello world",
-            # "location": ip.text.replace("\n", "")
         }),
     }
